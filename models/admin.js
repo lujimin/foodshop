4aed8e2e6a4ed8019a3c9354e8ff2f747719b553
mesterone <389129421@qq.com>
2015-05-09 23:46:35 +0800

    用户列表页面

--- models/admin.js
+++ models/admin.js
@@ -36,3 +36,34 @@ Admin.get = function(name, callback){//读取用户信息
     });
   });
 };
+
+Admin.getUserList = function(params, callback){
+  //打开数据库
+  mongodb.open(function(err, db){
+    if(err){
+      return callback(err);
+    }
+    //读取 admin 集合
+    db.collection('users', function(err, collection){
+      //数据库分页
+      var page = parseInt(params.page);
+      var size = parseInt(params.size);
+      page = (page-1)*size;
+      console.log(page + "  " + size);
+      if(err){
+        mongodb.close();
+        return callback(err);
+      }
+      //查找用户名 name 值为 name文档
+      collection.find().toArray(function(err, doc){
+        //console.log(doc);
+        mongodb.close();
+        if(doc){
+          callback(null,doc);//成功！返回查询的管理员信息
+        } else {
+          callback(err, null);//失败！返回null
+        }
+      });
+    });
+  });
+}
